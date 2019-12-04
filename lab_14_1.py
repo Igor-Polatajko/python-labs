3cf9ab6dd523009c77cc9d2dc4242346a53be4da
Ihor Polataiko <igor.bogdanovich39@gmail.com>
2019-12-04 14:10:02 +0200

    Finished lab 14_1

--- lab_14_1.py
+++ lab_14_1.py
@@ -5,14 +5,12 @@ ITEM_PRIORITY = {'1': 'HIGH (1)', '2': 'NORMAL (2)', '3': 'LOW (3)'}
 
 
 class ToDoItem:
-    current_item_id = 1
-
-    def __init__(self, item_id, title, content, priority):
+    def __init__(self, item_id, title, content, priority, completed):
         self.item_id = item_id
         self.title = title
         self.content = content
         self.priority = priority
-        self.completed = 'No'
+        self.completed = completed
 
     @staticmethod
     def db_store(method):
@@ -26,7 +24,7 @@ class ToDoItem:
     def db_read(method):
         def wrapper(*args, **kwargs):
             results = method(*args, **kwargs).fetchall()
-            return [ToDoItem(result[0], result[1], result[2], result[3]) for result in results]
+            return [ToDoItem(result[0], result[1], result[2], result[3], result[4]) for result in results]
 
         return wrapper
 
@@ -40,6 +38,8 @@ class DbConnector:
         return cls._instance
 
     def __init__(self):
+        if hasattr(self, 'db'):
+            return
         self.db = sqlite3.connect("todo_items")
         self.db.cursor().execute(
             'CREATE TABLE IF NOT EXISTS todo_items ('
@@ -67,8 +67,7 @@ class ToDoItemDao:
     @ToDoItem.db_store
     def create(self, item):
         self.db.cursor().execute("INSERT INTO todo_items (title, content, priority, completed)"
-                                 " VALUES (?, ?, ?, ?)", (item.title, item.content, item.priority, item.priority))
-        self.db.commit()
+                                 " VALUES (?, ?, ?, ?)", (item.title, item.content, item.priority, item.completed))
 
     @ToDoItem.db_store
     def update(self, item):
@@ -96,8 +95,8 @@ class View:
               'rm item_id - remove todo\n'
               'find field value - find by field value\n'
               'update item_id - find by field value\n'
-              'check item_id - mark as finished\n'
-              'uncheck item_id - unmark finished\n'
+              'complete item_id - mark as finished\n'
+              'uncomplete item_id - unmark finished\n'
               'help - see all the commands available\n'
               'exit - exit the program\n'
               '###############################################\n')
@@ -106,10 +105,12 @@ class View:
         old_title = ''
         old_content = ''
         old_priority = ''
+        completed = 'No'
         if item:
             old_title = f' ({item.title})'
             old_content = f' ({item.content})'
             old_priority = f' ({item.priority})'
+            completed = item.completed
 
         view_type = "Update" if item else "Add"
         print(f"### {view_type} item: ###")
@@ -122,7 +123,7 @@ class View:
                 break
             print("Error! Valid priority values: 1, 2, 3")
 
-        return ToDoItem(None, title, content, ITEM_PRIORITY[priority])
+        return ToDoItem(None, title, content, ITEM_PRIORITY[priority], completed)
 
     def show_list(self, data):
         if len(data) < 1:
@@ -182,17 +183,24 @@ class ViewHandler:
                 print("Incorrect command syntax!")
                 self._view.show_hint()
         elif i[0] == 'update':
-            if len(i) > 1:
-                item = self._todo_dao.find_by_field('id', i[1])
-                if item:
-                    updated_item = self._view.item_view(item[0])
-                    updated_item.item_id = item[0].item_id
-                    self._todo_dao.update(updated_item)
-                else:
-                    print('Item does not exist')
-            else:
-                print("Incorrect command syntax!")
-                self._view.show_hint()
+            def _update(item):
+                updated_item = self._view.item_view(item)
+                updated_item.item_id = item.item_id
+                self._todo_dao.update(updated_item)
+
+            self.__update(i, _update)
+        elif i[0] == 'complete':
+            def _check(item):
+                item.completed = 'YES'
+                self._todo_dao.update(item)
+
+            self.__update(i, _check)
+        elif i[0] == 'uncomplete':
+            def _uncheck(item):
+                item.completed = 'NO'
+                self._todo_dao.update(item)
+
+            self.__update(i, _uncheck)
         elif i[0] == 'find':
             if len(i) > 2:
                 field = i[1].lower()
@@ -207,6 +215,18 @@ class ViewHandler:
                 self._view.show_hint()
         else:
             print("Unknown command!")
+            self._view.show_hint()
+
+    def __update(self, req, update_handler):
+        if len(req) > 1:
+            item = self._todo_dao.find_by_field('id', req[1])
+            if item:
+                item = item[0]
+                update_handler(item)
+            else:
+                print('Item does not exist')
+        else:
+            print("Incorrect command syntax!")
             self._view.show_hint()
 
 
