5763d5ecd24dc052829336e31979d863dcc6d0ed
Igor-Polatajko <igor.bogdanovich39@gmail.com>
2019-12-05 00:28:48 +0200

    Finished lab 14_1_web

--- lab_14_1_web/app.py
+++ lab_14_1_web/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String
 
@@ -21,14 +21,19 @@ class ToDoItem(db.Model):
     completed = Column(String, unique=False, nullable=False)
 
 
+def _check_priority_validity(priority):
+    return priority in ('HIGH', 'NORMAL', 'LOW')
+
+
 @app.route('/')
 def index():
     field = request.args.get("field")
     value = request.args.get("value")
     if field is not None and value is not None:
         search_enabled = True
-        cls_field = getattr(ToDoItem, field)
-        items = ToDoItem.query.filter(cls_field == value or cls_field.contains(value)).all()
+        if hasattr(ToDoItem, field):
+            cls_field = getattr(ToDoItem, field)
+            items = ToDoItem.query.filter(cls_field == value or cls_field.contains(value)).all()
     else:
         search_enabled = False
         items = ToDoItem.query.all()
@@ -42,8 +47,10 @@ def add():
     if request.method == 'POST':
         if form.validate_on_submit():
             req_form = request.form
+            if not _check_priority_validity(req_form['priority']):
+                abort(400)
             item_to_store = ToDoItem(title=req_form['title'], content=req_form['content'],
-                                     priority=req_form['priority'], completed='No')
+                                     priority=req_form['priority'], completed='NO')
             db.session.add(item_to_store)
             db.session.commit()
             return redirect('/')
@@ -54,19 +61,44 @@ def add():
 
 @app.route('/edit/<item_id>', methods=['GET', 'POST'])
 def edit(item_id):
-    pass
+    form = AddEditForm()
+    error = {}
+    item = ToDoItem.query.filter_by(id=item_id).first()
+    if not item:
+        error = {'fatal': True, 'message': f'Item with id {item_id} not found'}
+    elif request.method == 'POST':
+        if form.validate_on_submit():
+            req_form = request.form
+            if not _check_priority_validity(req_form['priority']):
+                abort(400)
+            item.title = req_form['title']
+            item.content = req_form['content']
+            item.priority = req_form['priority']
+            db.session.commit()
+            return redirect('/')
+
+        else:
+            error = {'fatal': False, 'message': 'Fill all the fields!'}
+    return render_template('edit.html', item=item, form=form, error=error)
 
 
-@app.route('/items/<item_id>', methods=['DELETE'])
+@app.route('/delete/<item_id>', methods=['POST'])
 def delete(item_id):
     ToDoItem.query.filter(ToDoItem.id == item_id).delete()
     db.session.commit()
-    return '', 204
+    return redirect('/')
 
 
-@app.route('/items/<item_id>/<action>', methods=['POST'])
+@app.route('/state/<item_id>/<action>', methods=['POST'])
 def status_change(item_id, action):
-    return '', 200
+    item = ToDoItem.query.filter_by(id=item_id).first()
+    if item:
+        if action == 'complete':
+            item.completed = 'YES'
+        elif action == 'uncomplete':
+            item.completed = 'NO'
+    db.session.commit()
+    return redirect('/')
 
 
 if __name__ == '__main__':
